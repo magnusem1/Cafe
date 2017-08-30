89275fb9a32e513884e650e87f00026b569582e4
Magnus <Magnus@LAPTOP-J27TRD47>
2017-08-30 09:54:14 +0200

    Add project files.

---
+++ Cafe/Kaffe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe
+{
+    public abstract class Kaffe
+    {
+        public abstract int Pris();
+        public virtual string Styrke()
+        {
+            return "stærk";
+        }
+    }
+}
---
+++ CafeTests/CafeTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cafe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe.Tests
+{
+    [TestClass()]
+    public class CafeTests
+    {
+        [TestMethod()]
+        public void PrisTestKaffe()
+        {
+            //Arrange
+            var Cafe = new ();
+            //Act
+            int pris = Cafe.Pris();
+            //Assert
+            Assert.AreEqual(20, pris);
+        }
+
+        [TestMethod()]
+        public void StyrkeTestKaffe()
+        {
+            //Arrange
+            var cafe = new Kaffe();
+            //Act
+            string Styrke = cafe.Styrke();
+            //Assert
+            Assert.AreEqual("stærk", Styrke);
+        }
+    }
+}
