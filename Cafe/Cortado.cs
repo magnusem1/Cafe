85088974cec2cc51ba947d7c9670e69686cc4635
Magnus <Magnus@LAPTOP-J27TRD47>
2017-08-30 10:39:44 +0200

    Tilføjet interface IMælk

--- Cafe/Cortado.cs
+++ Cafe/Cortado.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace Cafe
 {
-    public class Cortado : Kaffe
+    public class Cortado : Kaffe , IMælk
     {
+        /// <summary>
+        /// Hvor meget mælk til en Cortado
+        /// </summary>
+        /// <returns></returns>
+        public int MlMælk()
+        {
+            return 25;
+        }
+
         /// <summary>
         /// Prisen på Cortado
         /// </summary>
--- Cafe/Latte.cs
+++ Cafe/Latte.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace Cafe
 {
-    public class Latte : Kaffe
+    public class Latte : Kaffe , IMælk
     {
+        public int MlMælk()
+        {
+            return 120;
+        }
+
         public override int Pris()
         {
             return 40;
